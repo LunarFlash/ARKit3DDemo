22947ae1d229f9bc8074e041ae134455c0da5fe3
Wang, Terry <terry.wang@accenture.com>
2017-12-19 21:45:29 -0500

    first commit

---
+++ ARKit3DDemo/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  AppCoda-ARKit-Part2
+//
+//  Created by Jayven Nhan on 23/10/17.
+//  Copyright © 2017 Jayven Nhan. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
+    
+}
