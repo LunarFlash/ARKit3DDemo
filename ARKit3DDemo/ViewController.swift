2906e76bfec20fc45e6ea4f789a731f12a8527c2
Wang, Terry <terry.wang@accenture.com>
2017-12-21 22:06:21 -0500

    add auto lighting to scene node

--- ARKit3DDemo/ViewController.swift
+++ ARKit3DDemo/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         addPaperPlane()
+        configureLighting()
         
     }
     
@@ -34,6 +35,14 @@ class ViewController: UIViewController {
         guard let paperPlaneScene = SCNScene(named: "paperPlane.scn"), let paperPlaneNode = paperPlaneScene.rootNode.childNode(withName: "paperPlane", recursively: true) else { return }
         paperPlaneNode.position = SCNVector3(x, y, z)
         sceneView.scene.rootNode.addChildNode(paperPlaneNode)
+    }
+    
+    func configureLighting() {
+        // SceneKit automatically adds lights to the scene. Technically, SceneKit automatically adds and places an omnidirectional light source when rendering scenes that contain no lights or only contain ambient lights.
+        sceneView.autoenablesDefaultLighting = true
+        /* the view automatically creates one or more SCNLight objects, adds them to the scene, and updates their properties to reflect estimated lighting information from the
+        camera scene. You would want to set this value to false if you want to directly control all lighting in the SceneKit scene.*/
+        sceneView.automaticallyUpdatesLighting = true
     }
 }
 
